bd761c1a4fd5797eac87ca79229df42fc34b3dd3
Vladyslav Petrenko <vladyslav533p@icloud.com>
2024-02-25 15:58:08 +0100

    Initial commit

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Vladyslav Petrenko on 25/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Vladyslav Petrenko on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

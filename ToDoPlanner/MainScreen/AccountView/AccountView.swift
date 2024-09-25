1b65d2a2bddd3ced639fc1d3337d24bee0242e8d
Vladyslav Petrenko <vladyslav533p@icloud.com>
2024-09-25 17:01:22 +0200

    - AccountView finished - AccountViewModel added

--- ToDoPlanner/MainScreen/AccountView/AccountView.swift
+++ ToDoPlanner/MainScreen/AccountView/AccountView.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var viewModel = AccountViewModel()
+    
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        ZStack(alignment: .top) {
             Color.charcoal
                 .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                Text(viewModel.userName)
+                    .font(.system(size: 27, weight: .bold, design: .default))
+                    .padding(.top, 16)
+                    .padding(.bottom, 4)
+                
+                Text(viewModel.userEmail)
+                    .font(.system(size: 20, weight: .regular, design: .default))
+                
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    RoundedButtonLabel(labelText: "Log out",
+                                       foregroundColor: .white,
+                                       backgroundColor: .red)
+                }
+            }
+            .padding()
         }
     }
 }
---
+++ ToDoPlanner/MainScreen/AccountView/AccountViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  AccountViewModel.swift
+//  ToDoPlanner
+//
+//  Created by Vladyslav Petrenko on 25/09/2024.
+//
+
+import SwiftUI
+
+@Observable
+final class AccountViewModel {
+    var userName = "Vladyslav Petrenko"
+    var userEmail = "vladyslav533p@icloud.com"
+}
